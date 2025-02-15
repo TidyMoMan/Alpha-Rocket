2ccef66f1bbc3349f0c977b0097e3ab3bf08c237
TidyMoMan <mosesdilts@gmail.com>
2025-02-15 18:45:15 -0500

    updated for parachute testing

--- Documents/Alpha-Rocket-Repo/src/main.cpp
+++ Documents/Alpha-Rocket-Repo/src/main.cpp
@@ -15,14 +15,14 @@
 #define G 9.80665
 #define TAKEOFF_THRESHOLD 20
 #define COAST_THRESHOLD 10
-#define DESCENT_THRESHOLD 0
+#define DESCENT_THRESHOLD 1
 #define ABORT_THRESHOLD 10 //non-spinal accel
 
 #define _PWM_LOGLEVEL_ 3
 #define SERVO_PIN 15
-#define FREQ 50 //servo frequency in HZ
-#define SERVO_OPEN_POS 10
-#define SERVO_CLOSED_POS 50
+#define FREQ 200 //servo frequency in HZ
+#define SERVO_OPEN_POS 33
+#define SERVO_CLOSED_POS 10
 
 #define BUZZER_PIN 3
 #define STATUS_LED_PIN 14
@@ -30,6 +30,7 @@
 
 #define DEBUG 1 //1 means debug mode is ON
 
+void updateState(float, float, float, float);
 float kalmanAlt();
 void deployChute();
 
@@ -39,8 +40,17 @@ Adafruit_ADXL375 accel = Adafruit_ADXL375(12345);
 RP2040_PWM* ServoPWM;
 RP2040_PWM* BuzzerPWM;
 
+typedef struct vehicleState{ //i just learned how to do this lol
+  float pos[3];
+  float vel[3];
+  float accel[3];
+};
+
+vehicleState currentState;
+
 float initialAlt;
-int servoState = SERVO_CLOSED_POS;
+
+int chuteDeployed = 0;
 
 int flightPhase = 0; 
 /*0 = on pad
@@ -48,6 +58,9 @@ int flightPhase = 0;
   2 = coasting
   3 = descent (hopefully this is when the parachute opens...)
   4 = landed.*/
+
+unsigned long current = 0;
+unsigned long last = 0;
 
 void setup(void)
 {
@@ -58,7 +71,7 @@ void setup(void)
   pinMode(BUZZER_PIN, OUTPUT);
   digitalWrite(STATUS_LED_PIN, LOW);
 
-  ServoPWM = new RP2040_PWM(SERVO_PIN, FREQ, servoState);
+  ServoPWM = new RP2040_PWM(SERVO_PIN, FREQ, SERVO_CLOSED_POS);
   BuzzerPWM = new RP2040_PWM(BUZZER_PIN, 1, 0);
 
   /*hold init until all sensors have been found on the bus*/
@@ -105,6 +118,7 @@ void setup(void)
   dataFile.close();
 
   Serial.println("\nInit success\n");
+
   //startup sounds are fun
   BuzzerPWM->setPWM(BUZZER_PIN, 750, 50);
   delay(100);
@@ -114,6 +128,9 @@ void setup(void)
   delay(400);
   BuzzerPWM->setPWM(BUZZER_PIN, 1150, 0);
 
+  //set servo to closed position (pedantic)
+  ServoPWM->setPWM(SERVO_PIN, FREQ, SERVO_CLOSED_POS);
+
   digitalWrite(STATUS_LED_PIN, HIGH);
 
 }
@@ -122,9 +139,8 @@ int isOn = 1;
 void loop(void)
 {
 
-  ServoPWM->setPWM(SERVO_PIN, FREQ, servoState);
 
-  if(count > 50 ){count = 0; isOn = !isOn; if(isOn){digitalWrite(STATUS_LED_PIN, LOW); BuzzerPWM->setPWM(BUZZER_PIN, 900, 50);}else{digitalWrite(STATUS_LED_PIN, HIGH); BuzzerPWM->setPWM(BUZZER_PIN, 1150, 75);}}
+  if(count > 50 ){count = 0; isOn = !isOn; if(isOn){digitalWrite(STATUS_LED_PIN, LOW); BuzzerPWM->setPWM(BUZZER_PIN, 900, 0);}else{digitalWrite(STATUS_LED_PIN, HIGH); BuzzerPWM->setPWM(BUZZER_PIN, 1150, 0);}}
   count++;
 
   sensors_event_t linearaccel, angleaccel, temp, HA_accel, temp_event, pressure_event;
@@ -134,15 +150,19 @@ void loop(void)
   baro.getEvents(&temp_event, &pressure_event);
 
   /*abort logic*/
-  if(flightPhase == 3 || (abs(HA_accel.acceleration.x) > ABORT_THRESHOLD || abs(HA_accel.acceleration.z) > ABORT_THRESHOLD) || (abs(linearaccel.acceleration.x) > ABORT_THRESHOLD || abs(linearaccel.acceleration.z) > ABORT_THRESHOLD)){
+  if(flightPhase == 3){ //if(flightPhase == 3 || (abs(HA_accel.acceleration.x) > ABORT_THRESHOLD || abs(HA_accel.acceleration.z) > ABORT_THRESHOLD) || (abs(linearaccel.acceleration.x) > ABORT_THRESHOLD || abs(linearaccel.acceleration.z) > ABORT_THRESHOLD)){
     deployChute();
   }
 
   /*flight phase logic*/
   if((linearaccel.acceleration.y > TAKEOFF_THRESHOLD && HA_accel.acceleration.y > TAKEOFF_THRESHOLD) && flightPhase == 0){flightPhase = 1;} //takeoff detection
   if((linearaccel.acceleration.y > COAST_THRESHOLD && HA_accel.acceleration.y > COAST_THRESHOLD) && flightPhase == 1){flightPhase = 2;} //coast detection
-  if((linearaccel.acceleration.y > DESCENT_THRESHOLD && HA_accel.acceleration.y > DESCENT_THRESHOLD) && flightPhase == 2){flightPhase = 3;} //descent detection
+  if((linearaccel.acceleration.y < DESCENT_THRESHOLD && HA_accel.acceleration.y < DESCENT_THRESHOLD) && flightPhase == 2){flightPhase = 3;} //descent detection
   //if(false){flightPhase = 4;} //landing detection
+
+  // current = micros();
+  // updateState(linearaccel.acceleration.x, linearaccel.acceleration.y, linearaccel.acceleration.z, (current-last));
+  // last = micros();
 
   /*logging, much room for improvement here*/
   if(!DEBUG){
@@ -225,15 +245,49 @@ void loop(void)
   Serial.print(">Baro Temp:");
   Serial.print(temp_event.temperature);
   Serial.print("C\n");
+
+
+  // Serial.print(">Estimated X pos:");
+  // Serial.print(currentState.pos[0]);
+  // Serial.print("M\n");
+
+  // Serial.print(">Estimated Y pos:");
+  // Serial.print(currentState.pos[1]);
+  // Serial.print("M\n");
+
+  // Serial.print(">Estimated Z pos:");
+  // Serial.print(currentState.pos[2]);
+  // Serial.print("M\n");
+
 }
 
+}
+
+void updateState(float accelx, float accely, float accelz, float dt){
+
+  dt = dt / 1000000; //convert microseconds to seconds.
+
+  currentState.accel[0] = accelx;
+  currentState.accel[1] = accely;
+  currentState.accel[2] = accelz;
+
+  currentState.vel[0] += currentState.accel[0] * dt;
+  currentState.vel[1] += (currentState.accel[1] - G) * dt;
+  currentState.vel[2] += currentState.accel[2] * dt;
+
+  currentState.pos[0] += currentState.vel[0] * dt;
+  currentState.pos[1] += currentState.vel[1] * dt;
+  currentState.pos[2] += currentState.vel[2] * dt;
 }
 
 float kalmanAlt(){
-  //calculus here... eventually
-  return baro.readAltitude() - initialAlt;
+  int baroAlt = baro.readAltitude() - initialAlt;
+  return 0.0;
 }
 
 void deployChute(){
-  servoState = SERVO_OPEN_POS;
+  if(!chuteDeployed){
+    chuteDeployed = 1; //yep, it's been deployed
+    ServoPWM->setPWM(SERVO_PIN, FREQ, SERVO_OPEN_POS);
+  }
 }
